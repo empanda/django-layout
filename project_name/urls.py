9725f7cd86a826efd163897c2f4c895fea7434f3
Johannas Heller <johann@phyfus.com>
2012-11-12 15:52:50 -0800

    Fix broken settings import and cleaned up urls.

--- project_name/urls.py
+++ project_name/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls.static import static
 from django.conf.urls.defaults import patterns, url, include
+from django.conf import settings
 
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
-   # (r'', include('{{ project_name }}.apps.')),
-    (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    (r'^admin/', include(admin.site.urls)),
+    # url(r'', include('{{ project_name }}.apps.')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 if settings.DEBUG and settings.MEDIA_ROOT:
