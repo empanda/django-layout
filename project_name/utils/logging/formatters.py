195652dbc915851b9c55315496721e39e0219065
Johannas Heller <johann@phyfus.com>
2012-11-21 03:03:13 -0800

    Added the logging formatter JSONFormatter into the utils.logging.formatters module.

---
+++ project_name/utils/logging/formatters.py
@@ -0,0 +1,96 @@
+import logging
+import json
+import datetime
+
+
+class JSONFormatter(logging.Formatter):
+    """
+    Formats LogRecords to JSON.
+
+    Instead of just dumping the entire LogRecord to JSON, first it moves all
+    the keys into a nested dictionary based on the format argument. You can
+    see the default layout below.
+
+    Note that the `extra` key will contain all the data that is passed to the
+    logger using the extra keyword parameter. For example:
+
+    logger.debug('message', extra={'ip': '127.0.0.1'})
+    """
+
+    default_layout = {
+        'logger': ['name', 'levelno', 'levelname'],
+        'file': ['pathname', 'filename', 'module', 'lineno', 'funcName'],
+        'time': ['created', 'asctime', 'relativeCreated'],
+        'process': ['thread', 'threadName', 'process', 'processName'],
+        'message': ['message', 'msg', 'args'],
+        'extra': 'all the extra parameters will go here',
+    }
+
+    default_fields = set(['relativeCreated', 'process', 'module', 'funcName',
+        'message', 'filename', 'levelno', 'processName', 'lineno', 'asctime',
+        'msg', 'args', 'exc_text', 'name', 'thread', 'created', 'threadName',
+        'msecs', 'pathname', 'exc_info', 'levelname'])
+
+    def __init__(self, layout=None, json_encoder=None):
+        """
+        You can specify two different options to custimize the final JSON
+        output.
+
+        layout - The layout of the final JSON object. You can reorganize this
+            to meet your needs.
+
+        json_encoder - The encoder class to use when converting objects into
+            there JSON compatible counterparts. You can override this to allow
+            for additional types to be stored in JSON.
+        """
+        self.layout = self.default_layout
+        if layout:
+            self.layout = layout
+
+        self.json_encoder = json_encoder
+
+    def process_extra(self, record):
+        """
+        Determines the difference between the default set of fields on a
+        LogRecord and the actual set of keys. These additional fields are the
+        `extra` fields for the LogRecord.
+        """
+        extra = {}
+        extra_keys = set(record.__dict__.keys()).difference(self.default_fields)
+        for key in extra_keys:
+            extra[key] = getattr(record, key)
+        return extra
+
+    def fill_layout(self, record, layout):
+        """
+        Returns a dictionary of nested dictionaries with data from the
+        LogRecord. It uses the layout dictionary defined in the format option
+        as the as the template to follow.
+        """
+        final = {}
+        for name, fields in layout.items():
+            if name is 'extra':
+                final['extra'] = self.process_extra(record)
+                continue
+            final[name] = {}
+            for field in fields:
+                final[name][field] = getattr(record, field)
+        return final
+
+    def formatDate(self, timestamp):
+        """
+        Format `asctime` into the standard ISO format.
+        """
+        dt = datetime.datetime.utcfromtimestamp(timestamp)
+        r = dt.isoformat() + 'Z'
+        return r
+
+    def format(self, record):
+        """
+        Add `message` and `asctime` to the LogRecord and then process the log
+        message into a dictionary which is returned a string of JSON.
+        """
+        record.message = record.getMessage()
+        record.asctime = self.formatDate(record.created)
+        log_data = self.fill_layout(record, self.layout)
+        return json.dumps(log_data, cls=self.json_encoder)
---
+++ project_name/utils/logging/tests.py
@@ -0,0 +1,134 @@
+from django.utils import unittest
+import json
+import decimal
+
+from .formatters import JSONFormatter
+
+
+class JSONFormatterTests(unittest.TestCase):
+    def test_out_is_valid_json(self):
+        """JSONFormatter should output valid JSON."""
+        formatter = JSONFormatter()
+        output = formatter.format(FakeLogRecord())
+
+        self.assertIsInstance(json.loads(output), dict)
+
+    def test_basic_usage(self):
+        """
+        JSONFormatter should be able to handle a simple LogRecord and return JSON for it.
+        """
+        expected_output = {
+            'extra': {'ip': '127.0.0.1', 'foo': 'asfd'},
+            'process': {'process': 7821, 'processName': 'MainProcess', 'threadName': 'MainThread', 'thread': 140735211397504},
+            'file': {'funcName': 'main', 'pathname': 'test.py', 'lineno': 116, 'module': 'test', 'filename': 'test.py'},
+            'time': {'relativeCreated': 8.126974105834961, 'asctime': '2012-11-20T23:30:59.111096Z', 'created': 1353454259.111096},
+            'logger': {'levelno': 10, 'name': 'main', 'levelname': 'DEBUG'},
+            'message': {'msg': 'in main function %s', 'message': "in main function ['foo']", 'args': ['foo']}
+        }
+
+        formatter = JSONFormatter()
+        output_json = formatter.format(FakeLogRecord())
+        output = json.loads(output_json)
+
+        self.assertEqual(output, expected_output)
+
+    def test_custom_layout(self):
+        """
+        JSONFormatter should use a custom dictionary format if specified.
+        """
+        expected_output = {
+            'file': {'funcName': 'main', 'lineno': 116, 'module': 'test'},
+            'time': {'asctime': '2012-11-20T23:30:59.111096Z'},
+            'logger': {'name': 'main', 'levelname': 'DEBUG'},
+            'message': {'message': "in main function ['foo']"}
+        }
+        custom_layout = {
+            'file': ['module', 'funcName', 'lineno'],
+            'time': ['asctime'],
+            'logger': ['name', 'levelname'],
+            'message': ['message']
+        }
+
+        formatter = JSONFormatter(layout=custom_layout)
+        output_json = formatter.format(FakeLogRecord())
+        output = json.loads(output_json)
+
+        self.assertEqual(output, expected_output)
+
+    def test_proccess_extra(self):
+        """
+        JSONFormatter.process_extra should find all the `extra` parameters on a LogRecord and return them as a dictionary.
+        """
+        expected_output = {'ip': '127.0.0.1', 'foo': 'asfd'}
+
+        formatter = JSONFormatter()
+        output = formatter.process_extra(FakeLogRecord())
+
+        self.assertEqual(output, expected_output)
+
+    def test_custom_json_encoder(self):
+        """
+        JSONFormatter should use a custom JSON encoder if it is specified.
+        """
+        expected_output = {'extra': {'ip': '127.0.0.1', 'foo': '101.03'}}
+        custom_layout = {'extra': ''}
+
+        formatter = JSONFormatter(
+            layout=custom_layout,
+            json_encoder=DecimalJSONEncoder
+        )
+
+        record = FakeLogRecord()
+        record.foo = decimal.Decimal('101.03')
+
+        output_json = formatter.format(record)
+        output = json.loads(output_json)
+
+        self.assertEqual(output, expected_output)
+
+    def test_formatDate(self):
+        """
+        JSONFormatter.formatDate should return a ISO formatted UTC date string.
+        """
+        expected_output = '2012-11-20T23:30:59.111096Z'
+        timestamp = 1353454259.111096
+
+        formatter = JSONFormatter()
+        output = formatter.formatDate(timestamp)
+
+        self.assertEqual(output, expected_output)
+
+
+class FakeLogRecord(object):
+    """A fake log record for testing."""
+    def __init__(self):
+        self.args = ['foo']
+        self.levelname = 'DEBUG'
+        self.levelno = 10
+        self.pathname = 'test.py'
+        self.filename = 'test.py'
+        self.process = 7821
+        self.processName = 'MainProcess'
+        self.threadName = 'MainThread'
+        self.thread = 140735211397504
+        self.funcName = 'main'
+        self.lineno = 116
+        self.module = 'test'
+        self.relativeCreated = 8.126974105834961
+        self.created = 1353454259.111096
+        self.name = 'main'
+        self.msg = 'in main function %s'
+        self.ip = '127.0.0.1'
+        self.foo = 'asfd'
+
+    def getMessage(self):
+        return self.msg % self.args
+
+
+class DecimalJSONEncoder(json.JSONEncoder):
+    """A JSON encoder that understands decimal objects."""
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return str(o)
+        else:
+            return super(DecimalJSONEncoder, self).default(o)
